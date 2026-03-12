11c6d2fdf23a278918fef2d2b7ef97b5de8b9604
aspc3t5 <dtbvang2010@gmail.com>
2026-03-12 08:30:13 +0700

    fix code

---
+++ DangThiBichVang/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DangThiBichVang.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+    }
+}

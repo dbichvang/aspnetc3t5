6979b42ec3d1fd86b25f4e2a3a244b6f0793754e
Đặng Thị Bích Vàng <dtbvang2010@gmail.com>
2026-04-05 13:41:33 +0700

    first commit

---
+++ bichvang_ASP/Program.cs
@@ -0,0 +1,33 @@
+﻿using bichvang_ASP.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 👉 Kết nối SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+// (có thể giữ hoặc bỏ, không ảnh hưởng nhiều lúc này)
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
